6a4e44a9fbd701727b283f0aefbca661350e4a26
Mel <melimore86@ufl.edu>
2020-03-26 17:33:08 -0400

    udpated the esri map to another map

--- fish_tracking/app.R
+++ fish_tracking/app.R
@@ -34,7 +34,7 @@ server <- function(input, output) {
    
   output$fishmap <- renderLeaflet({
     leaflet() %>% 
-      addProviderTiles("Esri")%>%
+      addProviderTiles("OpenTopoMap")%>%
       setView(-83.09, 29.25, 12)
   })
   
